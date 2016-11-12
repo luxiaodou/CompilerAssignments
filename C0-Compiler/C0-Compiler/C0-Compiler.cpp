63782ff2a62a83baf311004de0da72d013568f34
luxiaodou <lusongchn@gmail.com>
2016-11-12 23:30:25 +0800

    add C0-compiler & doc

---
+++ C0-Compiler/C0-Compiler/C0-Compiler.cpp
@@ -0,0 +1,11 @@
+// C0-Compiler.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include "global.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	lexer();
+	return 0;
+}
+
--- wordAnalyze/wordAnalyze/typDef.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TYPDEF_H_
-#define _TYPDEF_H_
-
-#define NUM_MAXLENGTH 9
-#define STRING_MAXLENGTH 500
-#define IDENT_MAXLENGTH 100
-
-#endif
